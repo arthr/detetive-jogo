55f4baaba927a9cb5d84d120c2f0b92b1057630a
Arthur Morais <arthrmrs@gmail.com>
2024-06-15 04:26:50 -0300

    feat: Adicionada lista de salas dinamicamente, ID para sala - A lista de salas agora é criada a partir das informações contidas no arquivo config.js - As salas agora possuem identificação única.

--- public/js/config.js
+++ public/js/config.js
@@ -11,16 +11,16 @@ const config = {
     doorsColor: 'rgba(255, 255, 255, 0.8)',
     secretPassageColor: 'rgba(255, 165, 0, 0.8)',
     rooms: [
-        { x: 0, y: 0, width: 7, height: 5, name: 'Restaurante', imageUrl: 'images/restaurante.webp', doors: [{ x: 7, y: 2, direction: 'left' }] },
-        { x: 10, y: 0, width: 6, height: 6, name: 'Prefeitura', imageUrl: 'images/prefeitura.webp', doors: [{ x: 11, y: 6, direction: 'up' }], secretPassage: { x: 12, y: 0, direction: 'up', destination: 'Estação de Trem' } },
-        { x: 19, y: 0, width: 6, height: 6, name: 'Banco', imageUrl: 'images/banco.webp', doors: [{ x: 21, y: 6, direction: 'up' }] },
-        { x: 0, y: 7, width: 6, height: 4, name: 'Hospital', imageUrl: 'images/hospital.webp', doors: [{ x: 6, y: 8, direction: 'left' }, { x: 1, y: 11, direction: 'up' }], secretPassage: { x: 0, y: 9, direction: 'left', destination: 'Mansão' } },
-        { x: 0, y: 12, width: 6, height: 6, name: 'Floricultura', imageUrl: 'images/floricultura.webp', doors: [{ x: 6, y: 14, direction: 'left' }, { x: 1, y: 18, direction: 'up' }] },
-        { x: 0, y: 20, width: 6, height: 5, name: 'Cemitério', imageUrl: 'images/cemiterio.webp', doors: [{ x: 6, y: 21, direction: 'left' }, { x: 3, y: 19, direction: 'down' }] },
-        { x: 9, y: 19, width: 7, height: 6, name: 'Estação de Trem', imageUrl: 'images/estacao-de-trem.webp', doors: [{ x: 8, y: 23, direction: 'right' }, { x: 16, y: 20, direction: 'left' }], secretPassage: { x: 12, y: 24, direction: 'down', destination: 'Prefeitura' } },
-        { x: 19, y: 8, width: 6, height: 4, name: 'Mansão', imageUrl: 'images/mansao.webp', doors: [{ x: 18, y: 10, direction: 'right' }, { x: 23, y: 7, direction: 'down' }], secretPassage: { x: 24, y: 10, direction: 'right', destination: 'Hospital' } },
-        { x: 19, y: 13, width: 6, height: 5, name: 'Hotel', imageUrl: 'images/hotel.webp', doors: [{ x: 18, y: 15, direction: 'right' }, { x: 24, y: 12, direction: 'down' }] },
-        { x: 19, y: 20, width: 6, height: 5, name: 'Boate', imageUrl: 'images/boate.webp', doors: [{ x: 18, y: 22, direction: 'right' }, { x: 23, y: 19, direction: 'down' }] },
-        { x: 9, y: 7, width: 7, height: 10, name: 'Praça Central', imageUrl: 'images/praca-central.webp', doors: [{ x: 8, y: 12, direction: 'right' }, { x: 16, y: 12, direction: 'left' }] }
+        { id: 1, x: 0, y: 0, width: 7, height: 5, name: 'Restaurante', imageUrl: 'images/restaurante.webp', doors: [{ x: 7, y: 2, direction: 'left' }] },
+        { id: 2, x: 10, y: 0, width: 6, height: 6, name: 'Prefeitura', imageUrl: 'images/prefeitura.webp', doors: [{ x: 11, y: 6, direction: 'up' }], secretPassage: { x: 12, y: 0, direction: 'up', destination: 'Estação de Trem' } },
+        { id: 3, x: 19, y: 0, width: 6, height: 6, name: 'Banco', imageUrl: 'images/banco.webp', doors: [{ x: 21, y: 6, direction: 'up' }] },
+        { id: 4, x: 0, y: 7, width: 6, height: 4, name: 'Hospital', imageUrl: 'images/hospital.webp', doors: [{ x: 6, y: 8, direction: 'left' }, { x: 1, y: 11, direction: 'up' }], secretPassage: { x: 0, y: 9, direction: 'left', destination: 'Mansão' } },
+        { id: 5, x: 0, y: 12, width: 6, height: 6, name: 'Floricultura', imageUrl: 'images/floricultura.webp', doors: [{ x: 6, y: 14, direction: 'left' }, { x: 1, y: 18, direction: 'up' }] },
+        { id: 6, x: 0, y: 20, width: 6, height: 5, name: 'Cemitério', imageUrl: 'images/cemiterio.webp', doors: [{ x: 6, y: 21, direction: 'left' }, { x: 3, y: 19, direction: 'down' }] },
+        { id: 7, x: 9, y: 19, width: 7, height: 6, name: 'Estação de Trem', imageUrl: 'images/estacao-de-trem.webp', doors: [{ x: 8, y: 23, direction: 'right' }, { x: 16, y: 20, direction: 'left' }], secretPassage: { x: 12, y: 24, direction: 'down', destination: 'Prefeitura' } },
+        { id: 8, x: 19, y: 8, width: 6, height: 4, name: 'Mansão', imageUrl: 'images/mansao.webp', doors: [{ x: 18, y: 10, direction: 'right' }, { x: 23, y: 7, direction: 'down' }], secretPassage: { x: 24, y: 10, direction: 'right', destination: 'Hospital' } },
+        { id: 9, x: 19, y: 13, width: 6, height: 5, name: 'Hotel', imageUrl: 'images/hotel.webp', doors: [{ x: 18, y: 15, direction: 'right' }, { x: 24, y: 12, direction: 'down' }] },
+        { id: 10, x: 19, y: 20, width: 6, height: 5, name: 'Boate', imageUrl: 'images/boate.webp', doors: [{ x: 18, y: 22, direction: 'right' }, { x: 23, y: 19, direction: 'down' }] },
+        { id: 11, x: 9, y: 7, width: 7, height: 10, name: 'Praça Central', imageUrl: 'images/praca-central.webp', doors: [{ x: 8, y: 12, direction: 'right' }, { x: 16, y: 12, direction: 'left' }] }
     ]
 };
---
+++ public/js/ui.js
@@ -0,0 +1,13 @@
+function createRoomList() {
+    const roomList = document.getElementById('room-list');
+    roomList.innerHTML = ''; // Limpar lista existente
+    config.rooms.forEach(room => {
+        const label = document.createElement('label');
+        label.innerHTML = `<input type="checkbox" value="${room.id}"> ${room.name}`;
+        roomList.appendChild(label);
+        roomList.appendChild(document.createElement('br'));
+    });
+}
+
+
+createRoomList();
