49561c61568d91180db859005c5641a191e1be95
Arthur Morais <arthrmrs@gmail.com>
2024-06-16 03:11:09 -0300

    chore: Removido comentário esquecido no config.js

--- src/config.js
+++ src/config.js
@@ -20,20 +20,3 @@ export const config = {
         { x: 9, y: 7, width: 7, height: 10, name: 'Praça Central', image: 'praca-central', doors: [{ x: 8, y: 12, direction: 'right' }, { x: 16, y: 12, direction: 'left' }] }
     ]
 };
-
-/**
- * rooms: [
-        { x: 0, y: 0, width: 7, height: 5, name: 'Restaurante', image: 'restaurante', doors: [{ x: 7, y: 2, direction: 'left' }] },
-        { x: 10, y: 0, width: 8, height: 6, name: 'Prefeitura', image: 'prefeitura', doors: [{ x: 11, y: 6, direction: 'up' }], secretPassage: { x: 12, y: 0, direction: 'up', destination: 'Estação de Trem' } },
-        { x: 21, y: 0, width: 7, height: 6, name: 'Banco', image: 'banco', doors: [{ x: 23, y: 6, direction: 'up' }] },
-        { x: 0, y: 7, width: 6, height: 4, name: 'Hospital', image: 'hospital', doors: [{ x: 6, y: 8, direction: 'left' }, { x: 1, y: 11, direction: 'up' }], secretPassage: { x: 0, y: 9, direction: 'left', destination: 'Mansão' } },
-        { x: 0, y: 12, width: 6, height: 6, name: 'Floricultura', image: 'floricultura', doors: [{ x: 6, y: 14, direction: 'left' }, { x: 1, y: 18, direction: 'up' }] },
-        { x: 0, y: 20, width: 6, height: 5, name: 'Cemitério', image: 'cemiterio', doors: [{ x: 6, y: 21, direction: 'left' }, { x: 3, y: 19, direction: 'down' }] },
-        { x: 9, y: 19, width: 7, height: 6, name: 'Estação de Trem', image: 'estacao-de-trem', doors: [{ x: 8, y: 23, direction: 'right' }, { x: 16, y: 20, direction: 'left' }], secretPassage: { x: 12, y: 24, direction: 'down', destination: 'Prefeitura' } },
-        { x: 22, y: 8, width: 6, height: 4, name: 'Mansão', image: 'mansao', doors: [{ x: 21, y: 10, direction: 'right' }, { x: 26, y: 7, direction: 'down' }], secretPassage: { x: 24, y: 10, direction: 'right', destination: 'Hospital' } },
-        { x: 19, y: 13, width: 6, height: 5, name: 'Hotel', image: 'hotel', doors: [{ x: 18, y: 15, direction: 'right' }, { x: 24, y: 12, direction: 'down' }] },
-        { x: 19, y: 20, width: 6, height: 5, name: 'Boate', image: 'boate', doors: [{ x: 18, y: 22, direction: 'right' }, { x: 23, y: 19, direction: 'down' }] },
-        
-        { x: 9, y: 7, width: 10, height: 7, name: 'Praça Central', image: 'praca-central', doors: [{ x: 8, y: 10, direction: 'right' }, { x: 19, y: 10, direction: 'left' }] }
-    ]
- */
