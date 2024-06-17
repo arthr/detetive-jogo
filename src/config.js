ce21bec53d75d3cd0ab9e66904b4a2abd1aa7d2d
Arthur Morais <arthrmrs@gmail.com>
2024-06-17 20:34:50 -0300

    stop: Ultima alteração, projeto descontinuado...

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const config = {
     coordinateColor: '#000',
     showTileBorders: false,
     tileBorderColorNumber: 0xff0000,
-    debugPathfinding: true,
+    debugPathfinding: false,
     rooms: [
         { x: 0, y: 0, width: 7, height: 5, name: 'Restaurante', image: 'restaurante', doors: [{ x: 7, y: 2, direction: 'left' }] },
         { x: 10, y: 0, width: 6, height: 6, name: 'Prefeitura', image: 'prefeitura', doors: [{ x: 11, y: 6, direction: 'up' }], secretPassage: { x: 12, y: 0, direction: 'up', destination: 'Estação de Trem' } },
